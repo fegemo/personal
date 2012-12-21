93517e224194e8620c7a06f9ca2c006d89f413c0
Flávio Roberto dos Santos Coutinho <fegemo@gmail.com>
2012-12-21 19:01:16 -0200

    Implemented a little bit of the internals of RomanNumber

--- merchanteer/src/main/java/com/guidetogalaxy/hitchhiker/merchanteer/currency/RomanNumber.java
+++ merchanteer/src/main/java/com/guidetogalaxy/hitchhiker/merchanteer/currency/RomanNumber.java
@@ -1,7 +1,12 @@
 package com.guidetogalaxy.hitchhiker.merchanteer.currency;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.ListIterator;
+import java.util.Vector;
 
 public final class RomanNumber {
 	private final List<RomanSymbol> representation;
@@ -15,13 +20,13 @@ public final class RomanNumber {
 	}
 	
 	private List<RomanSymbol> buildRomanNumberRepresentation(String candidateRepresentation) {
-		List<RomanSymbol> rep = new LinkedList<RomanSymbol>();
+		List<RomanSymbol> rep = new ArrayList<RomanSymbol>();
 		
 		
 		return rep;
 	}
 	
-	public static RomanNumber fromArabicFormat(String inArabic) throws CurrencyConversionException {
+	public static RomanNumber fromArabicFormat(String inArabic) throws CurrencyConversionException, MalformedCurrencyException {
 		// pre-conditions
 		try {
 			int intValue = Integer.parseInt(inArabic);
@@ -31,46 +36,159 @@ public final class RomanNumber {
 				throw new CurrencyConversionException("The provided arabic number candidate was bigger than what can be represented by Roman numbers.");
 			}
 		} catch (NumberFormatException ex) {
-			throw new MalformedConversionException("The provided arabic number candidate was not a recognized number.");
+			throw new MalformedCurrencyException("The provided arabic number candidate was not a recognized number.");
 		}
 		
 		RomanNumber roman = new RomanNumber(IDENTITY);
+		
 		char[] inArabicChars = inArabic.toCharArray();
 		char c;
-		int digitValue;
 		for (int index = 0; index < inArabicChars.length; index++) {
 			c = inArabicChars[index];
 			int digit = Character.getNumericValue(c);
 			if (digit == -1) {
 				throw new CurrencyConversionException("At least one digit of the arabic number was not a number.");
 			}
-			digitValue = digit * 10 ^ (inArabicChars.length - index - 1);
 			
+			roman.representation.addAll(convertArabicDigitIntoRoman(digit, index));
 		}
 		return roman;
+	}
+
+	/*
+	private static List<RomanSymbol> convertArabicNumberIntoRoman(int digit, int digitIndexInNumber) {
+		final RomanSymbol[] symbols = RomanSymbol.orderedValues();
+		List<RomanSymbol> roman = new ArrayList<RomanSymbol>();
+		RomanSymbol currentSymbol = null;
+		int lastUsedSymbolIndex = -1;
+		
+		while (number > 0) {
+			// Finds the first (descending order) symbol that is equal or less than the arabic number
+			for (int i = 0; i < symbols.length; i++) {
+				if (symbols[i].getIntValue() <= number) {
+					lastUsedSymbolIndex = i;
+					currentSymbol = symbols[i];
+					break;
+				}
+			}
+			
+			// If we can repeat still this symbol the necessary amount of times, we use it
+			int numberOfTimesAlreadyUsed = 0;
+			ListIterator<RomanSymbol> it = roman.listIterator(roman.size());
+			while (it.hasPrevious()) {
+				RomanSymbol s = it.previous();
+				if (s == currentSymbol) {
+					numberOfTimesAlreadyUsed++;
+				}
+			}
+			if (numberOfTimesAlreadyUsed + 1 <= currentSymbol.maxRepeat) {
+				roman.add(currentSymbol);
+				number -= currentSymbol.getIntValue();
+				continue;
+			}			
+			// Otherwise, we go back to the previous symbol and go with a subtraction
+			else {
+				
+			}
+			
+		}
+		
+		return roman;
+	}
+	*/
+	
+	private static List<RomanSymbol> convertArabicDigitIntoRoman(int digit, int magnitude) {
+		final RomanSymbol[] symbols = RomanSymbol.orderedValues();
+		List<RomanSymbol> roman = new ArrayList<RomanSymbol>();
+		
 	}
 	
 	@Override
 	public String toString() {
-		return representation.toUpperCase();
+		StringBuilder b = new StringBuilder();
+		for (RomanSymbol s : representation) {
+			b.append(s.toString().toUpperCase());
+		}
+		return b.toString();
 	}
 	
 	
 	private enum RomanSymbol {
-		I (1, 3),
-		V (5, 1),
-		X (10, 3),
-		L (50, 1),
-		C (100, 3),
-		D (500, 1),
-		M (1000, 3);
+		I (1,		RomanSymbolRole.REPEATER),
+		V (5, 		RomanSymbolRole.GUY_IN_THE_MIDDLE),
+		X (10,		RomanSymbolRole.REPEATER),
+		L (50,		RomanSymbolRole.GUY_IN_THE_MIDDLE),
+		C (100,		RomanSymbolRole.REPEATER),
+		D (500,		RomanSymbolRole.GUY_IN_THE_MIDDLE),
+		M (1000,	RomanSymbolRole.REPEATER);
 		
 		private final int intValue;
-		private final int maxRepeat;
+		private final RomanSymbolRole role;
+		private final int magnitude;
+		private static final RomanSymbolComparator DESC_COMPARATOR = new RomanSymbolComparator(false);
 		
-		RomanSymbol(int intValue, int maxRepeat) {
+		RomanSymbol(int intValue, RomanSymbolRole role) {
 			this.intValue = intValue;
-			this.maxRepeat = maxRepeat;
+			this.role = role;
+			int magnitude = -1;
+			while (intValue > 0) {
+				magnitude++;
+				intValue /= 10;
+			}
+			this.magnitude = magnitude;
+		}
+		
+		public int getIntValue() {
+			return intValue;
+		}
+		
+		public RomanSymbolRole getRole() {
+			return role;
+		}
+		
+		public int getMagnitude() {
+			return magnitude;
+		}
+		
+		public static RomanSymbol[] orderedValues() {
+			RomanSymbol[] values = RomanSymbol.values(); 
+			Arrays.sort(values, DESC_COMPARATOR);
+			return values;
+		}
+		
+		public static RomanSymbol[] getByMagnitude(int magnitude) {
+			RomanSymbol[] values = orderedValues();
+			List<RomanSymbol> valuesOfThisMagnitude = new ArrayList<RomanSymbol>(2);
+			for (RomanSymbol s : values) {
+				if (s.magnitude == magnitude) valuesOfThisMagnitude.add(s);
+			}
+			return (RomanSymbol[]) valuesOfThisMagnitude.toArray();
 		}
 	}
+	
+	static enum RomanSymbolRole {
+		REPEATER,			// Can appear 3 times and also reduce the next two (bigger) symbols
+		GUY_IN_THE_MIDDLE	// Only appears once at most, cannot reduce other symbols
+	}
+	
+	static class RomanSymbolComparator implements Comparator<RomanSymbol>
+	{
+		private final boolean ascending;
+		
+		RomanSymbolComparator(boolean ascending) {
+			this.ascending = ascending;
+		}
+		
+		public int compare(RomanSymbol o1, RomanSymbol o2) {
+			int orderResult = 0;
+			if (ascending) {
+				orderResult = o1.intValue - o2.intValue;
+			} else {
+				orderResult = o2.intValue - o1.intValue;
+			}
+			return orderResult;
+		}
+		
+	}
+	
 }
